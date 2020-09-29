a128fb37f7f281e2e1ed139ac517c4aec1a17509
AjibsBaba <samuelajibade22@gmail.com>
2020-09-29 23:24:10 +0100

    added template route

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'pollsapi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'rest_swagger'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
