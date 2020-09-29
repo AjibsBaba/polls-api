8f9410259145a4df6c380253c97233819bb69f1e
AjibsBaba <samuelajibade22@gmail.com>
2020-09-29 22:51:09 +0100

    fixed bugs

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from .views import PollViewSet
 from rest_framework import permissions
 from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
-from rest_framework_swagger.views import get_swagger_view
+from rest__swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Polls APi')
 
