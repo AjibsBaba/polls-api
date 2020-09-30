5cc65f32785c331d37a6a3d2e38947abd47fbbb1
AjibsBaba <samuelajibade22@gmail.com>
2020-09-30 19:17:05 +0100

    customized admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Poll, Choice
 
+# Registered Apps
 admin.site.register(Poll)
 admin.site.register(Choice)
+
+
+# Headers
+admin.site.site_header = "Polls Api Administration"
